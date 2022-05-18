6a9f70cc5446508498f2a4116d5f37712b7c2fb7
Kodihalli Srikar Siddarth <54308546+SrikarSiddarth@users.noreply.github.com>
2022-05-18 14:36:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-// import * from './codes/gameInput'
+// initialize variables
 
 var onclick = [];
 var onmouseover = [];
-var btns = []
+var btns = [];
+var side = 75;
+var inputWord = '';
+
+// puzzle variable stores the letter that form the puzzle
+var puzzle = [];
+
+for(var i=0; i<26; i++){
+	puzzle[i] = String.fromCharCode(65+Math.random()*26)
+}
+
 
 // wait for the page to load
 
@@ -12,12 +22,19 @@ window.onload = function(){
 		// get all the buttons
 		btns[i] = document.getElementById('b'+i);
 		btns[i].style.left = (i%4) * 80 + "px";
-		btns[i].style.top = Math.floor((i/4)) * 40 + 'px';
+		btns[i].style.top = Math.floor((i/4)) * 80 + 'px';
+		btns[i].style.width = side + 'px';
+		btns[i].style.height = side + 'px';
+		btns[i].innerHTML = puzzle[i]
 		// console.log('b'+i)
 	}
-	// console.log(btns)	
+	
+	archive = document.getElementById('archive');
+	displayField = document.getElementById('displayField');
 }
 
+
+// define callback functions
 
 function buttonClick(pos){
     for(var i=0;i<16;i++){
@@ -26,6 +43,8 @@ function buttonClick(pos){
     }
     onclick[pos] = 1;
     btns[pos].style.backgroundColor = 'orange';
+    inputWord += puzzle[pos];
+    displayField.innerHTML = inputWord;
     // btns[pos].style.color = 'white';
 
     // console.log('button: '+pos+' was pressed!')
@@ -35,6 +54,8 @@ function mouseover(pos){
 	if (onclick.includes(1)){
 		onmouseover[pos] = 1;
 		btns[pos].style.backgroundColor = 'orange';
+		inputWord += puzzle[pos];
+		displayField.innerHTML = inputWord;
 		// console.log(onclick)
   //   	console.log('button: '+pos+' was hovered upon!')	
 	}
@@ -42,7 +63,10 @@ function mouseover(pos){
     
 }
 
-// console.log(document.getElementsByClassName('canvas'))
+
+// document.body.addEventListener('mousedown', fucntion(){
+// 	displayField.innerHTML = inputWord;
+// });
 
 document.body.addEventListener('mouseup', function(){
     for(var i=0;i<16;i++){
@@ -50,7 +74,12 @@ document.body.addEventListener('mouseup', function(){
         onmouseover[i] = 0;
         btns[i].style.backgroundColor = 'skyblue';
     }
-    
+    if(inputWord){
+    	archive.innerHTML += inputWord + '<br>';
+    	inputWord = '';	
+    	displayField.innerHTML = '';
+    }    
     // console.log(onclick)
     // console.log('mouseup')
 });
+
